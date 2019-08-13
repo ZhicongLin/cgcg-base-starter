8972fbf9439c167e71957fb89be15986affa8ba2
linzhicong <linzhicong@miguantech.com>
2019-08-13 17:08:11 +0800

    添加swagger统一头部参数

--- cgcg-base-spring-boot-starter/src/main/java/com/cgcg/base/swagger/SwaggerConfig.java
+++ cgcg-base-spring-boot-starter/src/main/java/com/cgcg/base/swagger/SwaggerConfig.java
@@ -4,15 +4,22 @@ import com.cgcg.base.core.context.SpringContextHolder;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.PathProvider;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.AbstractPathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableSwagger2
 @Configuration
@@ -22,12 +29,20 @@ public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("token").description("客户端授权")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false).build();
+        pars.add(tokenPar.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(properties.getApis()))
                 .paths(PathSelectors.any())
-                .build();
+                .build().pathProvider(pathProvider())
+                .globalOperationParameters(pars);
     }
 
     private ApiInfo apiInfo() {
@@ -40,5 +55,19 @@ public class SwaggerConfig {
                 .description(properties.getDesc())
                 .version(properties.getVersion())
                 .build();
+    }
+
+    private PathProvider pathProvider() {
+        return new AbstractPathProvider() {
+            @Override
+            protected String applicationPath() {
+                return "/";
+            }
+
+            @Override
+            protected String getDocumentationPath() {
+                return "/";
+            }
+        };
     }
 }
