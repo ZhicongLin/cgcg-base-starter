81a6b91fff6d1dee34f7848833583f8dd8c5c3a5
linzhicong <linzhicong@miguantech.com>
2019-08-26 17:03:06 +0800

    修改国际化处理方式

--- cgcg-base-spring-boot-starter/src/main/java/com/cgcg/base/language/Translator.java
+++ cgcg-base-spring-boot-starter/src/main/java/com/cgcg/base/language/Translator.java
@@ -1,29 +1,26 @@
 package com.cgcg.base.language;
 
-import com.cgcg.base.core.context.SpringContextHolder;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
+@Component
 public class Translator {
 
-    private static ResourceBundleMessageSource messageSource;
+    private static MessageSource messageSource;
+
+    public Translator(@Autowired MessageSource messageSource) {
+        Translator.messageSource = messageSource;
+    }
 
     public static String toLocale(String msg) {
         if (StringUtils.isBlank(msg)) {
             return null;
         }
-        if (messageSource == null) {
-            try {
-                messageSource = SpringContextHolder.getBean(ResourceBundleMessageSource.class);
-            } catch (NoSuchBeanDefinitionException e) {
-                return msg;
-            }
-        }
-
         final Locale locale = LocaleContextHolder.getLocale();
         return messageSource.getMessage(msg, null, locale);
     }
