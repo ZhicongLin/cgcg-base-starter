887c04b9959ba4b2036dd9325cd572f19fb60896
ZhicongLin <892972899@qq.com>
2022-02-07 17:17:12 +0800

    添加mongodb的操作

--- cgcg-mongodb-spring-boot-starter/src/main/java/com/cgcg/mongo/file/FileRepository.java
+++ cgcg-mongodb-spring-boot-starter/src/main/java/com/cgcg/mongo/file/FileRepository.java
@@ -1,11 +1,9 @@
 package com.cgcg.mongo.file;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author zhicong.lin
  */
-@Repository
 public interface FileRepository extends MongoRepository<FileGridFsInfo, String> {
 }
